381767686f1a7bffa5e30d3749a1fcc9cb6c0f35
bomkeen <bomkeendata@gmail.com>
2018-08-28 12:42:01 +0700

    'encryp'

--- md5.php
+++ md5.php
@@ -1,3 +1,5 @@
 <?php
-echo base64_encode('$database = "MATULEE2";$hostname = "10.252.5.21";$user = "apptmea";$password = "apptmea";');
+$dd= base64_encode('$database = "MATULEE2";$hostname = "10.252.5.21";$user = "apptmea";$password = "apptmea";');
+echo base64_decode($dd);
+
 ?>
